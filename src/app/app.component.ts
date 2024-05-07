d7c97179f2c3c193c7c2cdf167a20abda2d46552
KdevSoluctions <gustavokarnopp.tech@gmail.com>
2024-05-06 22:53:40 -0300

    feat/Construindo estrutura sobre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,23 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './features/pages/home/home.component';
 import { HeaderNavComponent } from './features/components/header-nav/header-nav.component';
+import { FooterComponent } from './features/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [RouterOutlet, HomeComponent, HeaderNavComponent],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    HeaderNavComponent,
+    FooterComponent
+  ],
   template: `
   <router-outlet id="app">
-  <app-header-nav></app-header-nav>
+    <app-header-nav></app-header-nav>
   </router-outlet>
+  <app-footer></app-footer>
   `,
 })
 export class AppComponent {
--- src/app/features/components/header-nav/header-nav.component.ts
+++ src/app/features/components/header-nav/header-nav.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    CommonModule
+  ],
   templateUrl: './header-nav.component.html',
   styleUrl: './header-nav.component.scss'
 })
 export class HeaderNavComponent {
+  opened = false;
+  selectRoute: string = '';
 
+  constructor(
+    private router: Router,
+  ){
+    this.router.events.subscribe((events: any) => {
+      if(events instanceof NavigationEnd){
+        this.selectRoute = this.router.url
+      }
+    })
+  }
 }
