6a89dd88a63a57a9d7ab863d6fa9b781bb11a6d8
KdevSoluctions <gustavokarnopp.tech@gmail.com>
2024-05-06 23:39:07 -0300

    feat/Ajustes

--- src/app/features/components/footer/footer.component.ts
+++ src/app/features/components/footer/footer.component.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    CommonModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
