132192e5103aea1da85d27bc754c4d3dab8c619c
KdevSoluctions <gustavokarnopp.tech@gmail.com>
2024-06-04 17:37:43 -0300

    feat/Tentando melhorar o desempenho dos Gifs

--- src/app/features/components/projetos/projeto-agendamentos/projeto-agendamentos.component.ts
+++ src/app/features/components/projetos/projeto-agendamentos/projeto-agendamentos.component.ts
@@ -12,20 +12,4 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 export class ProjetoAgendamentosComponent {
   @Input() imageUrlGif1: string = '';
   @Input() isImageLoadedGif1: boolean = false;
-  // imageUrlGif1 = 'https://buck-gifs-portfolio-1.s3.us-east-2.amazonaws.com/gifprojetos.gif';
-  // isImageLoadedGif1: boolean  = false;
-
-  // constructor(
-  //   private filtersGlobalsService: FiltersGlobalsService
-  // ) {}
-
-  // ngOnInit(): void {
-
-  //   //! Valida se a imagem ja carregou para apresentar se nao persiste o skeleton
-  //   this.filtersGlobalsService.checkImageLoad(this.imageUrlGif1).then(isLoaded => {
-  //     this.isImageLoadedGif1 = isLoaded;
-  //   }).catch(error => {
-  //     console.log(error, 'Error loading image');
-  //   });
-  // }
 }
--- src/app/features/components/projetos/projeto-diet/projeto-diet.component.ts
+++ src/app/features/components/projetos/projeto-diet/projeto-diet.component.ts
@@ -15,21 +15,4 @@ export class ProjetoDietComponent {
   @Input() imageUrlGif2: string = '';
   @Input() isImageLoadedGif2: boolean = false;
   // public projetoDiet = environment.PROJETO_DIET
-
-  // imageUrlGif2 = 'https://buck-gifs-portfolio-1.s3.us-east-2.amazonaws.com/gifprojetosDiet.gif';
-  // isImageLoadedGif2: boolean  = false;
-
-  // constructor(
-  //   private filtersGlobalsService: FiltersGlobalsService
-  // ) {}
-
-  // ngOnInit(): void {
-
-  //   //! Valida se a imagem ja carregou para apresentar se nao persiste o skeleton
-  //   this.filtersGlobalsService.checkImageLoad(this.imageUrlGif2).then(isLoaded => {
-  //     this.isImageLoadedGif2 = isLoaded;
-  //   }).catch(error => {
-  //     console.log(error, 'Error loading image');
-  //   });
-  // }
 }
